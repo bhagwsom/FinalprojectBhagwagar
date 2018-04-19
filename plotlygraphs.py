79d64bce308914ce551d4cf3db8d462c0f42e399
bhagwsom <35805443+bhagwsom@users.noreply.github.com>
2018-04-18 23:11:15 -0400

    there was an error that I fixed in the params

---
+++ plotlygraphs.py
@@ -0,0 +1,38 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+def plot_graph1(trace1, trace2):
+    #compare trackcount for 2 Albums
+
+    data= [go.Bar(y=trace1, x=trace2, name='Bar Chart Comparing TrackCount to Beyonce')]
+    print(data)
+
+
+    py.plot(data, filename='Bar Chart Comparing Track Count')
+
+def plot_graph2(trace1, trace2):
+    data= [go.Bar(y=trace1, x=trace2, name='Bar Chart Comparing Popularity to Beyonce')]
+    print(data)
+
+
+    py.plot(data, filename='Bar Chart Comparing Popularity')
+
+
+
+
+def plot_graph3(trace1, trace2):
+    data= [go.Bar(y=trace1, x=trace2, name='Bar Chart Comparing Follower Count to Beyonce')]
+    print(data)
+
+
+    py.plot(data, filename='Bar Chart Comparing FollowerCount')
+
+
+def plot_graph4(trace1, trace2):
+    #compare popularity for two artists
+    data= [go.Bar(y=trace1, x=trace2, name='Bar Chart Comparing Follower Count to Beyonce')]
+    print(data)
+
+
+    py.plot(data, filename='Bar Chart Comparing Popularity')
